f5632de782b7a8e6a209a5ec144989ca3571b6d1
S. M. MAJIDUL ISLAM <smmajidulislam77@gmail.com>
2025-07-11 22:40:54 +0600

    [added error]

--- client/src/app/features/product/productApi.js
+++ client/src/app/features/product/productApi.js
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
 const productApi = createApi({
   reducerPath: "productApi",
   baseQuery: fetchBaseQuery({
     baseUrl: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
     credentials: "include",
   }),
+
   tagTypes: ["Product"],
   endpoints: (builder) => ({
     getProducts: builder.query({
